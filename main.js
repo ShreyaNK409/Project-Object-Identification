ea21c998531f21af76d5a9a33f782b46a500a6ea
ShreyaNK409 <78596107+ShreyaNK409@users.noreply.github.com>
2021-07-12 16:45:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function cr()
+{
+    window.location="cr.html";
+}
+
+function bs()
+{
+    window.location="bs.html";
+}
+
+function sr()
+{
+    window.location="sr.html";
+}
+
+function br()
+{
+    window.location="br.html";
+}
+
+function hr()
+{
+    window.location="h.html";
+}
+
+function g()
+{
+    window.location="g.html";
+}
